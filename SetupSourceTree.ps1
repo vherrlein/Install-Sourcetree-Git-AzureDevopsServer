95e35bbdc015eb1a3fbcd451b61826cbc50d5269
Vladimir Herrlein <vherrlein@gmail.com>
2020-05-07 11:27:02 +0200

    Replace BITS Transfert with WebClient .Net object in order to fix SSL Verification checks in case of self-signed certificate.

--- SetupSourceTree.ps1
+++ SetupSourceTree.ps1
@@ -82,6 +82,86 @@ function New-ShellLink{
     $Shortcut.Arguments = $ArgumentsToSourceExe
     $Shortcut.Save()
 }
+function Invoke-DownloadFile{
+    param(
+        [Parameter(Mandatory=$true)]
+        $url, 
+        [Parameter(Mandatory=$true)]
+        $destination, 
+        [switch]$includeStats
+    )
+    $global:downloadCompleted=$false
+    $wc = New-Object Net.WebClient
+    $wc.UseDefaultCredentials = $Global:webSession.UseDefaultCredentials
+    $wc.Credentials = $Global:webSession.Credentials
+    $wc.Proxy = $Global:webSession.Proxy
+
+    $file = $url | Split-Path -Leaf
+    
+    $start = Get-Date 
+    $null = Register-ObjectEvent -InputObject $wc -EventName DownloadProgressChanged `
+        -MessageData @{start=$start;includeStats=$includeStats;url=$url} `
+        -SourceIdentifier WebClient.DownloadProgressChanged -Action { 
+            filter Get-FileSize {
+	            "{0:N2} {1}" -f $(
+	            if ($_ -lt 1kb) { $_, 'Bytes' }
+	            elseif ($_ -lt 1mb) { ($_/1kb), 'KB' }
+	            elseif ($_ -lt 1gb) { ($_/1mb), 'MB' }
+	            elseif ($_ -lt 1tb) { ($_/1gb), 'GB' }
+	            elseif ($_ -lt 1pb) { ($_/1tb), 'TB' }
+	            else { ($_/1pb), 'PB' }
+	            )
+            }
+            $elapsed = ((Get-Date) - $event.MessageData.start)
+            #calculate average speed in Mbps
+            $averageSpeed = ($EventArgs.BytesReceived * 8 / 1MB) / $elapsed.TotalSeconds
+            $elapsed = $elapsed.ToString('hh\:mm\:ss')
+            #calculate remaining time considering average speed
+            $remainingSeconds = ($EventArgs.TotalBytesToReceive - $EventArgs.BytesReceived) * 8 / 1MB / $averageSpeed
+            $receivedSize = $EventArgs.BytesReceived | Get-FileSize
+            $totalSize = $EventArgs.TotalBytesToReceive | Get-FileSize        
+            Write-Progress -Activity (" $($event.MessageData.url) {0:N2} Mbps" -f $averageSpeed) `
+                -Status ("{0} of {1} ({2}% in {3})" -f $receivedSize,$totalSize,$EventArgs.ProgressPercentage,$elapsed) `
+                -SecondsRemaining $remainingSeconds `
+                -PercentComplete $EventArgs.ProgressPercentage
+            if ($EventArgs.ProgressPercentage -eq 100){
+                 Write-Progress -Activity (" $url {0:N2} Mbps" -f $averageSpeed) `
+                -Status 'Done' -Completed
+                if ($event.MessageData.includeStats.IsPresent){
+                    ([PSCustomObject]@{Name='Invoke-DownloadFile';TotalSize=$totalSize;Time=$elapsed}) | Out-Host
+                }
+            }
+            return;
+        } 
+    $null = Register-ObjectEvent -InputObject $wc -EventName DownloadFileCompleted `
+         -MessageData @{destination=$destination;client=$wc} `
+         -SourceIdentifier WebClient.DownloadFileCompleted -Action { 
+            
+            Get-Item $event.MessageData.destination | Unblock-File | Out-Null
+
+            $global:downloadCompleted = $true;
+
+            $event.MessageData.client.Dispose();
+            return;
+        }  
+    try  {  
+        $task = $wc.DownloadFileAsync($url, $destination)
+        
+        while(-not $global:downloadCompleted){
+            
+            # Waiting end of the download
+        }
+        
+    }  
+    catch [System.Net.WebException]  {  
+        Write-Error "Download of $url failed" -ErrorAction Stop
+    }   
+    finally  {    
+        $wc.Dispose()
+        Unregister-Event -SourceIdentifier WebClient.DownloadProgressChanged -Force | Out-Null
+        Unregister-Event -SourceIdentifier WebClient.DownloadFileCompleted -Force | Out-Null
+    }  
+ }
 
 ## TFS Methods
 function Get-TFSContext($collectionUrl){
@@ -241,12 +321,14 @@ function Install-Git([bool]$latest){
         $wc = New-Object System.Net.WebClient
         $wc.UseDefaultCredentials = $true
         
-        $wc.DownloadFile($version.url,$fileFullName)
+        Invoke-DownloadFile $version.url $fileFullName
 
         #Install
         Write-Warning "`tFollow the installation wizard`r`nthen the setup process will continue."
         Start-Sleep -Seconds 2
-        Start-Process $fileFullName -Wait
+        $p = Start-Process $fileFullName -PassThru
+        do {start-sleep -Milliseconds 500}
+        until ($p.HasExited)
 
     }
 }
@@ -545,12 +627,14 @@ function Install-SourceTree([bool]$latest){
         $fileName = Split-Path $version.url -Leaf
         $fileFullName = "$env:TEMP\$fileName"
         Remove-Item $fileFullName -Force -ErrorAction 0;
-        Start-BitsTransfer -Source $version.url -Destination $fileFullName
+        Invoke-DownloadFile $version.url $fileFullName
 
         #Install
         Write-Warning "`tFollow the installation wizard, when you arrive at the step to register, close the installation wizard`r`nthen the setup process will continue."
         Start-Sleep -Seconds 2
-        Start-Process $fileFullName -Wait
+        $p = Start-Process $fileFullName -PassThru
+        do {start-sleep -Milliseconds 500}
+        until ($p.HasExited)
         #Start-Sleep -Seconds 5
         kill -Name pageant -ErrorAction 0 -Force
 
